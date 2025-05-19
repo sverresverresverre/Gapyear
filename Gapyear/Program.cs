0f0499f6e48b45ebf1e3ff41b9789f6fcc74cb84
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2025-05-19 09:13:38 +0200

    jfiejf

--- Gapyear/Program.cs
+++ Gapyear/Program.cs
@@ -4,6 +4,7 @@
 Gapyear gap = new Gapyear();
 
 Console.WriteLine("1. Get a job?");
+Console.WriteLine("2. Don't get a job?");
 
 string doWhat = Console.ReadLine();
 if (doWhat == "1")
@@ -12,8 +13,12 @@ if (doWhat == "1")
     gap.Job();
     gap.Stats();
 }
-else
+else if (doWhat == "2")
 {
     gap.Stats();
+}
+else
+{
+    Console.WriteLine("Not a legit answer, start over");
 }
 Console.ReadLine();
--- Gapyear/obj/Debug/net9.0/Gapyear.AssemblyInfo.cs
+++ Gapyear/obj/Debug/net9.0/Gapyear.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gapyear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ddc02f10b4428ae61bfbc13337e308ad0cb770c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gapyear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gapyear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
